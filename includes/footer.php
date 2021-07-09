0fc71e66b956508de7c9a049d73859796c12a50a
Shadowfist <72393544+shadowfist04@users.noreply.github.com>
2021-07-09 18:18:45 +0530

    Delete includes directory

--- includes/footer.php
+++
@@ -1,8 +0,0 @@
-  
-  
-  <footer class="py-5 bg-inverse">
-        <div class="container">
-            <p class="m-0 text-center text-white" style="font-weight: bold;font-size:40px;">**Tech Avengers**</p>
-        </div>
-        <!-- /.container -->
-    </footer>
