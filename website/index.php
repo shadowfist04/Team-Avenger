1cfa7f662b31b77b1d32e9d8ad2e96e0ef6520f8
SanthoshPatkar <72272748+SanthoshPatkar@users.noreply.github.com>
2021-07-09 17:39:45 +0530

    home page

---
+++ website/index.php
@@ -0,0 +1,130 @@
+<?php
+error_reporting(0);
+include('includes/config.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
+
+    <title>BloodBank & Donor Management System</title>
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="css/modern-business.css" rel="stylesheet">
+    <style>
+    .navbar-toggler {
+        z-index: 1;
+    }
+   
+    
+    @media (max-width: 576px) {
+        nav > .container {
+            width: 100%;
+        }
+    }
+    .carousel-item.active,
+    .carousel-item-next,
+    .carousel-item-prev {
+        display: block;
+    }
+    </style>
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+<?php include('includes/header.php');?>
+<?php include('includes/slider.php');?>
+<hr>
+   
+    <!-- Page Content -->
+    <div class="container">
+
+        <h1 class="my-4 title">Welcome to BloodBank & Donor Management System</h1>
+        
+
+        <!-- Portfolio Section -->
+        <h2 class="title">Some of the Donar</h2>
+
+        <div class="row">
+                   <?php 
+$status=1;
+$sql = "SELECT * from tblblooddonars where status=:status order by rand() limit 6";
+$query = $dbh -> prepare($sql);
+$query->bindParam(':status',$status,PDO::PARAM_STR);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $result)
+{ ?>
+
+            <div class="col-lg-4 col-sm-6 portfolio-item">
+                <div class="card h-100">
+                    <a href="#"><img class="card-img-top img-fluid" src="images/blood-donor.jpg" alt="" ></a>
+                    <div class="card-block con-head">
+                        <h4 class="card-title"><a href="#"><?php echo htmlentities($result->FullName);?></a></h4>
+<p class="card-text"><b>  Gender :</b> <?php echo htmlentities($result->Gender);?></p>
+<p class="card-text"><b>Blood Group :</b> <?php echo htmlentities($result->BloodGroup);?></p>
+<p class="card-text"><b>District :</b> <?php echo htmlentities($result->district);?></p>
+
+                    </div>
+                </div>
+            </div>
+
+            <?php }} ?>
+           </div>
+        <!-- /.row -->
+<hr>
+        <!-- Features Section -->
+        <div class="row" style="background-color:white;">
+            <div class="col-lg-6" style="margin-top:5%; ">
+                <h2 style="margin-top: 3%;">BLOOD GROUPS</h2>
+                <ul style="margin-top:5%;">
+                
+                
+<li>A positive or A negative</li>
+<li>B positive or B negative</li>
+<li>O positive or O negative</li>
+<li>AB positive or AB negative.</li>
+                </ul>
+               
+            </div>
+            <div class="col-lg-6">
+                <img class="img-fluid rounded" src="images/blood-donor (1).jpg" alt="">
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Call to Action Section -->
+            <div class="col-md-4" style="margin-bottom: 2%; text-align:center; margin-left:350px; ">
+                <a class="btn btn-lg btn-secondary btn-block" href="become-donar.php" style="text-align:center;">Become a Donar</a>
+            </div>
+        
+
+    </div>
+  
+
+    <!-- Footer -->
+  <?php include('includes/footer.php');?>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/tether/tether.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
