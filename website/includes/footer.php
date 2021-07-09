9313e79f817fe594c6cf41a234f6c9bd0c31567c
SanthoshPatkar <72272748+SanthoshPatkar@users.noreply.github.com>
2021-07-09 23:08:50 +0530

    updated footer

--- website/includes/footer.php
+++ website/includes/footer.php
@@ -2,7 +2,7 @@
   
   <footer class="py-5 bg-inverse">
         <div class="container">
-            <p class="m-0 text-center text-white" style="font-weight: bold;font-size:40px;">**Tech Avengers**</p>
+            <p class="m-0 text-center" style="font-weight: bold;font-size:40px; color: #c7daaf75;">**Tech Avengers**</p>
         </div>
         <!-- /.container -->
     </footer>
