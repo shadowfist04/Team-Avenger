b527fb4380008ee1f5f538a2b89732f6a62795c7
Shadowfist <72393544+shadowfist04@users.noreply.github.com>
2021-07-09 18:08:19 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+  
+  
+  <footer class="py-5 bg-inverse">
+        <div class="container">
+            <p class="m-0 text-center text-white" style="font-weight: bold;font-size:40px;">**Tech Avengers**</p>
+        </div>
+        <!-- /.container -->
+    </footer>
---
+++ includes/slider.php
@@ -0,0 +1,11 @@
+ <header>
+    <div class="banner" style="background-image: url('images/banner2.jpg'); background-size:cover;">
+        <div class="overlay">
+            <div class="banner-text">
+               <h2 class="slogan">*Every Blood Donor Is a Life Saver*</h2>
+                
+            </div>
+        </div>
+
+    </div>
+    </header>
